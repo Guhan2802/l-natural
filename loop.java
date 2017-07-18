353dd0f81c63642543cf4256e605d6fdaee1bdfc
Guhan2802 <k.guhan1997@gmail.com>
2017-07-18 15:36:43 +0530

    Update and rename l-natural.java to loop.java

---
+++ loop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class loop
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int sum=0;
+int n=nextInt();
+for(int i=0;i<n;i++)
+{
+sum+=i;
+}}}
