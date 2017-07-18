fadb8eef8d0cfce25a95685ca9ffc2180c33f85d
Guhan2802 <k.guhan1997@gmail.com>
2017-07-18 15:33:41 +0530

    Create l-natural.java

---
+++ l-natural.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class l-natural
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int sum=0,n=s.nextInt();
+for(int i=0;i<n;i++)
+{
+sum+=i;
+}
+}
+}
